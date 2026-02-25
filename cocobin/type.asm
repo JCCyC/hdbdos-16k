ea1915a3d8e741ad3b53c84777553b9a34d4e990
Juan Carlos Castro y Castro <jccyc1965@gmail.com>
2026-02-24 22:37:43 -0300

    A program to "TYPE" (or "cat") a text file

---
+++ cocobin/type.asm
@@ -0,0 +1,35 @@
+* LBB2F  -> fp(X)->FPA1
+* LBC14  -> fp(X)->FPA0
+* LBC35  -> FPA0->fp(X)
+* LB740  -> FPA0->X (0-65535)
+* LB4F3  -> ACCB->FPA0 (0-255)
+* GIVABF -> ACCD->FPA0 (0-65535)
+
+
+ROM16K		equ	1
+		USE	ecb_equates.asm
+
+		org	$7C00
+entry		bra	start
+
+start		lda	DEVNUM
+		pshs	a
+
+charloop	lda	#1
+		sta	DEVNUM
+		jsr	LA176
+		tst	CINBFL
+		bne	typeend
+
+		tfr	a,b
+		lda	#0
+		sta	DEVNUM
+		tfr	b,a
+		jsr	[CHROUT]
+		bra	charloop
+
+typeend		puls	a
+		sta	DEVNUM
+		rts
+
+		end	entry
